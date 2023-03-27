2a72320b7623958382d8d24ace448c5fc49b7ecd
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-03-28 00:34:56 +0200

    Env file added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "../models/Deck";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
@@ -12,9 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 5000;
 
 const connectDB = async () => {
-  const { connection } = await mongoose.connect(
-    "mongodb+srv://bijen1234:bijen1234@cluster0.j8pc21b.mongodb.net/deck-app?retryWrites=true&w=majority"
-  );
+  const { connection } = await mongoose.connect(process.env.MONGO_URL!);
 
   console.log(`MongoDb connected on host: ${connection.host}`);
 };
