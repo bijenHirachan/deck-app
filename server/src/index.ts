9f6c8172ac54ef91dea712b6052d7cd595372ef0
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-03-28 17:48:41 +0200

    Showing the decks

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,18 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import Deck from "../models/Deck";
 import { config } from "dotenv";
+import cors from "cors";
 
 config();
 
 const app = express();
 
 //middlewares
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,9 +26,16 @@ const connectDB = async () => {
 };
 connectDB();
 
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+
+  return res.status(200).json({
+    decks,
+  });
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
   const { title } = req.body;
-
   const deck = await Deck.create({ title });
 
   return res.status(201).json({
